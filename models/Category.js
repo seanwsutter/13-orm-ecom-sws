7543fd320f0a4e0cffb3d2d1e428e86bc243ad81
seanwsutter <seanwsutter@gmail.com>
2023-11-14 20:52:43 -0600

    setup files & models

---
+++ models/Category.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("'../config/connection.js'");
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  
+  
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "category",
+  }
+);
+
+module.exports = Category;
+
+
+// `Category`
+//   * `id`
+//   * Integer.
+//     * Doesn't allow null values.
+//   * Set as primary key.
+//     * Uses auto increment.
+  // * `category_name`
+  // * String.
+  //   * Doesn't allow null values.
---
+++ models/index.js
@@ -0,0 +1,46 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+}),
+
+// Categories have many Products
+  
+  Category.hasMany(Product, {
+    foreignKey: "category_id",
+    onDelete: "CASCADE"
+  })
+
+// Products belongToMany Tags (through ProductTag)
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+})
+
+// Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
+
+
+// `Product` belongs to`Category`, and`Category` has many `Product` models, as a category can have multiple products but a product can only belong to one category.
+
+// * `Product` belongs to many `Tag` models, and`Tag` belongs to many `Product` models.Allow products to have multiple tags and tags to have many products by using the `ProductTag` through model.
